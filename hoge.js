f0cb6de68e9a2da455bd69e47e08ae3219d9a08a
m_ishikawa <m_ishikawa@corp.allabout.co.jp>
2017-12-14 15:29:50 +0900

    もろもろ練習、まずは公式のまま

---
+++ hoge.js
@@ -0,0 +1,52 @@
+// Cache APIが一部未実装なのでポリフィルをロード
+importScripts('serviceworker-cache-polyfill.js');
+
+// キャッシュのキーとなる文字列
+var CACHE_KEY = 'service-worker-playground-v1';
+
+self.addEventListener('install', function (e) {
+
+  console.log('ServiceWorker.oninstall: ', e);
+
+  e.waitUntil(
+    caches.open(CACHE_KEY).then(function (cache) {
+
+      // cacheさせたいリクエストのキーを追加
+      return cache.addAll([
+        'https://imgcp.aacdn.jp/img-a/800/auto/aa/gm/article/4/6/0/1/6/9/201711171604/washhands.jpg',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+
+  console.log('ServiceWorker.onfetch: ', e);
+
+  e.respondWith(
+    caches.open(CACHE_KEY).then(function (cache) {
+      return cache.match(e.request).then(function (response) {
+        if (response) {
+
+          // e.requestに対するキャッシュが見つかったのでそれを返却
+          return response;
+        } else {
+
+          // キャッシュが見つからなかったので取得
+          fetch(e.request.clone()).then(function (response) {
+
+            // 取得したリソースをキャッシュに登録
+            cache.put(e.request, response.clone());
+
+            // 取得したリソースを返却
+            return response;
+          });
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('ServiceWorker.onactivate: ', e);
+});
